4247ef3c9af916b041734258ec73be8a65671dd2
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-06-16 21:45:58 +0200

    add snack 2

--- js/app.js
+++ js/app.js
@@ -51,3 +51,55 @@ document.getElementById('bici').innerHTML += ul;
 
 
 
+/**
+ * Snack2
+ * Creare un array di oggetti di squadre di calcio. 
+ * Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. 
+ * Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0. 
+ * Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti. 
+ * (Utilizzate le arrow function). Infine usando la destrutturazione creiamo un nuovo array di oggetti i cui elementi sono sempre degli oggetti che contengono solo nomi e falli subiti. 
+ * Stampiamo tutto in console.
+ */
+
+const squadre = [
+    {
+        nome: 'Juve',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Italia',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: 'Roma',
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+];
+
+const newSquadre = [];
+
+//Arrow function generazione di numeri random interi
+const numRnd = (min,max) => Math.floor(Math.random() * max - min + 1) + min;
+
+//Arrow function creazione di oggetto
+const setGenerateObj = (obj, ...property) => {
+    const newObj = {};
+    for(let i = 0; i < property.length; i++){
+        newObj[property[i]] = obj[property[i]];
+    }
+    return newObj;
+};
+
+
+
+for(let i = 0; i < squadre.length; i++){
+    squadre[i].puntiFatti = numRnd(1,20);
+    squadre[i].falliSubiti = numRnd(1,20);
+    //Invochiamo la funzione che crea l'oggetto dinamicamente
+    newSquadre.push(setGenerateObj(squadre[i], 'nome','falliSubiti'));
+}
+
+console.log(newSquadre);
