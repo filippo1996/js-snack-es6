9cac896f96e71ff276426f733d4147bd5f3d5a76
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-06-16 20:07:27 +0200

    add snack 1

--- js/app.js
+++ js/app.js
@@ -5,3 +5,49 @@
  * nome e peso. 
  * Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
  */
+
+// Array di oggetti
+const bici = [
+    {
+        nome: 'Mont',
+        peso: 12
+    },
+    {
+        nome: 'Scoot',
+        peso: 5
+    },
+    {
+        nome: 'Speed',
+        peso: 8
+    }
+];
+
+/**
+ * Metodo 1
+ */
+let minPeso = bici[0];
+
+for(let i = 1; i < bici.length; i++){
+    if(bici[i].peso < minPeso.peso){
+        minPeso = bici[i];
+    }
+}
+
+//Utilizziamo il destructuring dell'oggetto minPeso
+const {nome, peso} = minPeso;
+
+console.log(nome, peso);
+
+//Mostriamo i valori delle costanti tramite template literal backticks ``
+const ul = 
+`<ul>
+    <li>Nome della bici è ${nome}</li>
+    <li>Il suo peso è di ${peso} Kg</li>
+</ul>`; 
+
+document.getElementById('bici').innerHTML += ul;
+
+
+
+
+
